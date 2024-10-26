cfd19152cab0ab6cc81498a7281fd0cca7dd203f
Ewik <ewikk77@gmail.com>
2024-10-26 21:14:25 +0200

    added static methods for general use

--- EcgData.py
+++ EcgData.py
@@ -115,43 +115,47 @@ class EcgData:
         return
 
     def __find_r_peaks(self):
-        self.__r_peaks = PanTompkins.find_r_peaks(self.raw_data, self.frequency)
+        self.__r_peaks = self.find_r_peaks(self.raw_data, self.frequency)
         return self.__r_peaks
-    
+
+    @staticmethod
+    def find_r_peaks(data: np.ndarray, frequency: int) -> np.ndarray:
+        return PanTompkins.find_r_peaks(data, frequency)
+
     def __find_new_r_peaks(self):
         if len(self.raw_data) < self.frequency * 2:
             return
         if len(self.__r_peaks) == 0:
             self.__find_r_peaks()
             return
-        
+
         index = -1
-        for i in range(len(self.raw_data)-1, -1, -1):
+        for i in range(len(self.raw_data) - 1, -1, -1):
             if self.raw_data[i][0] < self.__r_peaks[-1][0]:
                 index = i
                 break
-        #offset = int(self.__mean_rr/2*self.frequency)
+        # offset = int(self.__mean_rr/2*self.frequency)
         # new_peaks = PanTompkins.find_r_peaks(self.raw_data[-(index+offset):], self.frequency)
         new_peaks = PanTompkins.find_r_peaks(self.raw_data[-index:], self.frequency)
-        
+
         start_index = -1
         for i in range(len(new_peaks)):
             if self.__r_peaks[-1][0] < new_peaks[i][0]:
                 start_index = i
                 break
-            
+
         if -1 == start_index:
             return
-        
-        
-        if self.raw_data[-1][0] - new_peaks[-1][0] > self.__mean_rr/2:
-            end_index = len(new_peaks-1)
+
+        rr_intervals = self.calc_rr_intervals(self.__r_peaks[-4:])
+        last_3_mean_rr = self.calc_mean_rr(rr_intervals)
+
+        if self.raw_data[-1][0] - new_peaks[-1][0] > last_3_mean_rr / 4:
+            end_index = len(new_peaks - 1)
         else:
             end_index = -1
-        
+
         self.__r_peaks = np.vstack((self.__r_peaks, new_peaks[start_index:end_index]))
-
-
 
     def __find_r_peaks_piotr(self):
         self.__r_peaks_piotr = NAME_THIS_MODULE_YOURSELF_PIOTER.find_r_peaks_piotr(
@@ -160,56 +164,62 @@ class EcgData:
         return self.__r_peaks_piotr
 
     def __calc_rr_intervals(self):
-        if len(self.__r_peaks) < 2:
+        self.__rr_intervals = self.calc_rr_intervals(self.__r_peaks)
+        return self.__rr_intervals
+
+    @staticmethod
+    def calc_rr_intervals(r_peaks: np.ndarray) -> np.ndarray:
+        if len(r_peaks) < 2:
             return np.empty(0)
 
-        self.__rr_intervals = np.diff([peak[0] for peak in self.__r_peaks])
-        return self.__rr_intervals
-    
-    # @staticmethod
-    # def calc_rr_intervals(r_peaks: np.ndarray)->np.ndarray:
-    #     if len(r_peaks) < 2:
-    #         return np.empty(0)
-
-    #     rr_intervals = np.diff([peak[0] for peak in r_peaks])
-    #     return rr_intervals
+        rr_intervals = np.diff([peak[0] for peak in r_peaks])
+        return rr_intervals
 
     def __calc_mean_rr(self):
-        self.__mean_rr = (
-            np.mean(self.__rr_intervals) if len(self.__rr_intervals) > 0 else None
-        )
+        self.__mean_rr = self.calc_mean_rr(self.__rr_intervals)
         return self.__mean_rr
 
+    @staticmethod
+    def calc_mean_rr(rr_intervals: np.ndarray) -> np.ndarray:
+        return np.mean(rr_intervals) if len(rr_intervals) > 0 else None
+
     def __calc_sdnn(self):
-        self.__sdnn = (
-            np.std(self.__rr_intervals) * 1e3 if len(self.__rr_intervals) > 0 else None
-        )
+        self.__sdnn = self.calc_sdnn(self.__rr_intervals)
         return self.__sdnn
 
+    @staticmethod
+    def calc_sdnn(rr_intervals: np.ndarray) -> float:
+        return np.std(rr_intervals) * 1e3 if len(rr_intervals) > 0 else None
+
     def __calc_rmssd(self):
-        diff_rr_intervals = np.diff(self.__rr_intervals)
-        self.__rmssd = (
+        self.__rmssd = self.calc_rmssd(self.__rr_intervals)
+        return  self.__rmssd
+
+    @staticmethod
+    def calc_rmssd(rr_intervals: np.ndarray) -> float:
+        diff_rr_intervals = np.diff(rr_intervals)
+        return (
             np.sqrt(np.mean(diff_rr_intervals**2)) * 1e3
             if len(diff_rr_intervals) > 0
             else None
         )
-        return self.__rmssd
 
     def __calc_pnn50(self):
-        if len(self.__rr_intervals) < 2:
-            self.__pnn50 = None
-            return self.__pnn50
+        self.__pnn50 = self.calc_pnn50(self.__rr_intervals)
+        return self.__pnn50
 
-        diff_rr_intervals = np.diff(self.__rr_intervals)
+    @staticmethod
+    def calc_pnn50(rr_intervals: np.ndarray) -> float:
+        if len(rr_intervals) < 2:
+            return None
+
+        diff_rr_intervals = np.diff(rr_intervals)
 
         nn50_count = np.sum(np.abs(diff_rr_intervals) > 0.050)  # 50 ms
-
-        self.__pnn50 = (
+        return (
             (nn50_count / len(diff_rr_intervals)) * 100
             if len(diff_rr_intervals) > 0
             else None
         )
-
-        return self.__pnn50
 
     pass
