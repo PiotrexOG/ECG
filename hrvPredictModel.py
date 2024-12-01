2c98ce7b7909c095f02ce0b720b049d493d59a11
PiotrexOG <118371452+PiotrexOG@users.noreply.github.com>
2024-12-01 15:39:23 +0100

    k folding

--- hrvPredictModel.py
+++ hrvPredictModel.py
@@ -9,6 +9,7 @@ from tensorflow.keras.layers import (
     Dropout,
     BatchNormalization,
 )
+
 from EcgData import EcgData
 from config import *
 from Finders.PanTompkinsFinder import PanTompkinsFinder
@@ -23,9 +24,12 @@ from EcgPlotter import EcgPlotter
 import os
 
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv1D, MaxPooling1D, LSTM, Dense, Dropout, BatchNormalization, Flatten, GlobalAveragePooling1D, MultiHeadAttention
+from tensorflow.keras.layers import Input, Conv1D, MaxPooling1D, LSTM, Dense, Dropout, BatchNormalization, Flatten, GlobalAveragePooling1D, MultiHeadAttention, Multiply
 from tensorflow.keras.layers import GlobalMaxPooling1D, Concatenate
 from tensorflow.keras.regularizers import l2
+
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 def create_hrv_model(input_length, output_len):
     input_signal = Input(shape=(input_length, 1))
@@ -63,39 +67,42 @@ def se_block(input_tensor, ratio=16):
 
 
 # Model definition
-def new_model(input_length):
-    input_signal = tf.keras.Input(shape=(input_length, 1))
+def new_model(input_length, output_len):
+   input_signal = Input(shape=(input_length, 1))  # 1300 próbek, 1 kanał
 
 
-    x = tf.keras.layers.Conv1D(filters=32, kernel_size=15, activation='relu', padding='same')(input_signal)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = se_block(x)
-    x = tf.keras.layers.MaxPooling1D(pool_size=25)(x)
+   # x = Conv1D(filters=8, kernel_size=11, activation='relu', padding='same')(input_signal)
+   # x = BatchNormalization()(x)
+   # x = MaxPooling1D(pool_size=10)(x)  # 15000 → 3750
 
-    x = tf.keras.layers.Conv1D(filters=64, kernel_size=11, activation='relu', padding='same')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = se_block(x)
-    x = tf.keras.layers.MaxPooling1D(pool_size=15)(x)
+   x = Conv1D(filters=32, kernel_size=15, activation='relu', padding='same')(input_signal)
+   x = BatchNormalization()(x)
+   x = MaxPooling1D(pool_size=5)(x)
 
-    # Separate branches for SDNN
-    sdnn_branch = tf.keras.layers.Conv1D(filters=128, kernel_size=7, activation='relu', padding='same')(x)
-    sdnn_branch = tf.keras.layers.BatchNormalization()(sdnn_branch)
-    sdnn_branch = tf.keras.layers.GlobalAveragePooling1D()(sdnn_branch)
-    sdnn_branch = tf.keras.layers.Dense(64, activation='relu')(sdnn_branch)
-    sdnn_branch = tf.keras.layers.Dropout(0.2)(sdnn_branch)
-    sdnn_output = tf.keras.layers.Dense(1, activation='linear', name='sdnn')(sdnn_branch)
+   x = Conv1D(filters=64, kernel_size=11, activation='relu', padding='same')(x)
+   x = BatchNormalization()(x)
+   x = MaxPooling1D(pool_size=5)(x)
 
-    # Separate branches for RMSSD
-    rmssd_branch = tf.keras.layers.Conv1D(filters=128, kernel_size=7, activation='relu', padding='same')(x)
-    rmssd_branch = tf.keras.layers.BatchNormalization()(rmssd_branch)
-    rmssd_branch = tf.keras.layers.GlobalAveragePooling1D()(rmssd_branch)
-    rmssd_branch = tf.keras.layers.Dense(64, activation='relu')(rmssd_branch)
-    rmssd_branch = tf.keras.layers.Dropout(0.2)(rmssd_branch)
-    rmssd_output = tf.keras.layers.Dense(1, activation='linear', name='rmssd')(rmssd_branch)
+   x = Conv1D(filters=128, kernel_size=9, activation='relu', padding='same')(x)
+   x = BatchNormalization()(x)
+   x = MaxPooling1D(pool_size=4)(x)
 
-    # Model
-    model = tf.keras.Model(inputs=input_signal, outputs={'sdnn': sdnn_output, 'rmssd': rmssd_output})
-    return model
+   # Attention Layer
+   x_attention = Dense(128, activation='sigmoid')(x)
+   x = Multiply()([x, x_attention])
+
+   x = GlobalAveragePooling1D()(x)
+   x = Dense(128, activation='relu')(x)
+   x = Dropout(0.4)(x)
+   x = Dense(64, activation='relu')(x)
+   x = Dropout(0.3)(x)
+
+   # Wyjścia: SDNN i RMSSD
+   output = Dense(output_len, activation='linear')(x)
+   # Model
+   model = Model(inputs=input_signal, outputs=output)
+   return model
+
 
 def with_lstm(input_length, output_len):
     input_signal = Input(shape=(input_length, 1))
@@ -230,19 +237,19 @@ def create_scatter_line_plotsc(metric_name, y_true, y_pred, save_path=None):
     plt.legend()
     if save_path:
         plt.savefig(os.path.join(save_path, f"{metric_name}_scatter.png"))
-    plt.show()
+    #plt.show(block = False)
 
-    # Line Plot: Comparison Over Time
-    plt.figure(figsize=(10, 6))
-    plt.plot(y_true, label=f"True {metric_name}")
-    plt.plot(y_pred, label=f"Predicted {metric_name}", linestyle='--')
-    plt.title(f"{metric_name}: True vs Predicted (Line)")
-    plt.xlabel("Sample Index")
-    plt.ylabel(f"{metric_name} Value")
-    plt.legend()
-    if save_path:
-        plt.savefig(os.path.join(save_path, f"{metric_name}_line.png"))
-    plt.show()
+    # # Line Plot: Comparison Over Time
+    # plt.figure(figsize=(10, 6))
+    # plt.plot(y_true, label=f"True {metric_name}")
+    # plt.plot(y_pred, label=f"Predicted {metric_name}", linestyle='--')
+    # plt.title(f"{metric_name}: True vs Predicted (Line)")
+    # plt.xlabel("Sample Index")
+    # plt.ylabel(f"{metric_name} Value")
+    # plt.legend()
+    # if save_path:
+    #     plt.savefig(os.path.join(save_path, f"{metric_name}_line.png"))
+    # plt.show()
 
 def create_scatter_plot(axs, metric: str, real, pred):
     axs.scatter(real, pred, alpha=0.5)
@@ -326,126 +333,171 @@ if __name__ == "__main__":
     #     metrics=["mae"],
     # )
 
-    random_seed = 42
+    kf = KFold(n_splits=5, shuffle=True)  # 5-krotna walidacja krzyżowa
 
-    X, y = get_patients_data_csv("data", [csvs[2], csvs[3], csvs[4]], finder)#, int(0.8*input_length))
+    # Listy do przechowywania wyników dla każdej iteracji
+    mae_sdnn_list, mse_sdnn_list, rmse_sdnn_list, r2_sdnn_list = [], [], [], []
+    mae_rmssd_list, mse_rmssd_list, rmse_rmssd_list, r2_rmssd_list = [], [], [], []
 
-    X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=0.3, random_state=random_seed
-    )
+    X, y = get_patients_data_csv("data", [csvs[2], csvs[3], csvs[4]], finder)  # , int(0.8*input_length))
+    random_seed = None
+    for fold, (train_idx, val_idx) in enumerate(kf.split(X)):
+        print(f"\n### Fold {fold + 1} ###")
 
-    # Przygotowanie wyjść dla modelu wielowymiarowego
-    y_train = {'sdnn': y_train[:, 0], 'rmssd': y_train[:, 1]}
-    y_val = {'sdnn': y_val[:, 0], 'rmssd': y_val[:, 1]}
+        X_train, X_val, y_train, y_val = train_test_split(
+            X, y, test_size=0.3
+        )
 
-    # Parametry modelu
-    input_length = X.shape[1]
-    model = new_model(input_length)
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.015),
-        loss={'sdnn': 'mse', 'rmssd': 'mse'},
-        metrics={'sdnn': 'mae', 'rmssd': 'mae'}
-    )
+        # Parametry modelu
+        input_length = X.shape[1]
+        model = new_model(input_length, 2)
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=0.0008),
+            loss='mse',
+            metrics=['mae']
+        )
 
-    # model_path = "models\\test.keras"
-    #model_path = f"models\\{"_".join(METRICS).replace("\\","").replace("/", "")}_{input_length}_{random_seed}_{finder.__class__.__name__}.keras"
-    model_path = f"models\\siemka.keras"
+        # # model_path = "models\\test.keras"
+        model_path = f"models\\{fold}{"_".join(METRICS).replace("\\","").replace("/", "")}_{input_length}_{random_seed}_{finder.__class__.__name__}.keras"
+        # #model_path = f"models\\siemka.keras"
 
-    checkpoint = tf.keras.callbacks.ModelCheckpoint(
-        model_path, monitor="loss", verbose=1, save_best_only=True, mode="min"
-    )
-    callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=50)
+        checkpoint = tf.keras.callbacks.ModelCheckpoint(
+            model_path, monitor="loss", verbose=1, save_best_only=True, mode="min"
+        )
+        callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=65)
 
-    epochs= 250
+        epochs= 250
 
-    # history = model.fit(
-    #     X_train,
-    #     y_train,
-    #     validation_data=(X_val, y_val),
-    #     epochs=epochs,
-    #     batch_size=16,
-    #     verbose=1,
-    #     shuffle=True,
-    #     callbacks=[checkpoint, callback],
-    # )
+        history = model.fit(
+            X_train,
+            y_train,
+            validation_data=(X_val, y_val),
+            epochs=epochs,
+            batch_size=16,
+            verbose=1,
+            shuffle=True,
+            callbacks=[checkpoint, callback],
+        )
 
-    # X_test = X_val
-    # y_test = y_val
-    #
-    # model.load_weights(model_path)
-    # pred_result = model.predict(X_test)
-    # # pred_result2 = model.predict(X1)
-    #
-    # mae = mean_absolute_error(y_test, pred_result)
-    #
-    # mse = mean_squared_error(y_test, pred_result)
-    #
-    # rmse = np.sqrt(mse)
-    #
-    # r2 = r2_score(y_test, pred_result)
-    #
-    # print(f"MAE: {mae:.2f}")
-    # print(f"MSE: {mse:.2f}")
-    # print(f"RMSE: {rmse:.2f}")
-    # print(f"R^2: {r2:.2f}")
-    #
-    # save_path = None
-    #
-    # for metric in METRICS:
-    #     create_scatter_line_plots(
-    #         metric,
-    #         y_test[:, METRICS.index(metric)],
-    #         pred_result[:, METRICS.index(metric)],
-    #         save_path,
-    #     )
-    #
-    #
-    # plt.show()
+        # X_test = X_val
+        # y_test = y_val
+        #
+        # model.load_weights(model_path)
+        # pred_result = model.predict(X_test)
+        # # pred_result2 = model.predict(X1)
+        #
+        # mae = mean_absolute_error(y_test, pred_result)
+        #
+        # mse = mean_squared_error(y_test, pred_result)
+        #
+        # rmse = np.sqrt(mse)
+        #
+        # r2 = r2_score(y_test, pred_result)
+        #
+        # print(f"MAE: {mae:.2f}")
+        # print(f"MSE: {mse:.2f}")
+        # print(f"RMSE: {rmse:.2f}")
+        # print(f"R^2: {r2:.2f}")
+        #
+        # save_path = None
+        #
 
-    # Testowanie modelu
-    model.load_weights(model_path)
-    pred_result = model.predict(X_val)
 
-    # Rozpakowanie wyników
-    sdnn_pred = pred_result['sdnn']
-    rmssd_pred = pred_result['rmssd']
+        # Testowanie modelu
+        model.load_weights(model_path)
+        # pred_result = model.predict(X_val)
+        #
+        # # Rozpakowanie wyników
+        # sdnn_pred = pred_result['sdnn']
+        # rmssd_pred = pred_result['rmssd']
+        #
+        # # Prawdziwe wartości
+        # sdnn_test = y_val['sdnn']
+        # rmssd_test = y_val['rmssd']
+        #
+        # # Metryki dla SDNN
+        # mae_sdnn = mean_absolute_error(sdnn_test, sdnn_pred)
+        # mse_sdnn = mean_squared_error(sdnn_test, sdnn_pred)
+        # rmse_sdnn = np.sqrt(mse_sdnn)
+        # r2_sdnn = r2_score(sdnn_test, sdnn_pred)
+        #
+        # # Metryki dla RMSSD
+        # mae_rmssd = mean_absolute_error(rmssd_test, rmssd_pred)
+        # mse_rmssd = mean_squared_error(rmssd_test, rmssd_pred)
+        # rmse_rmssd = np.sqrt(mse_rmssd)
+        # r2_rmssd = r2_score(rmssd_test, rmssd_pred)
+        #
+        # # Wyświetlanie wyników
+        # print("### SDNN ###")
+        # print(f"MAE: {mae_sdnn:.2f}")
+        # print(f"MSE: {mse_sdnn:.2f}")
+        # print(f"RMSE: {rmse_sdnn:.2f}")
+        # print(f"R^2: {r2_sdnn:.2f}")
+        #
+        # print("### RMSSD ###")
+        # print(f"MAE: {mae_rmssd:.2f}")
+        # print(f"MSE: {mse_rmssd:.2f}")
+        # print(f"RMSE: {rmse_rmssd:.2f}")
+        # print(f"R^2: {r2_rmssd:.2f}")
+        #
+        # # # Wizualizacja wyników
 
-    # Prawdziwe wartości
-    sdnn_test = y_val['sdnn']
-    rmssd_test = y_val['rmssd']
+        #
+        # # Wywołanie funkcji dla SDNN i RMSSD
+        # Gcreate_plots("SDNN", sdnn_test, sdnn_pred)
+        # Gcreate_plots("RMSSD", rmssd_test, rmssd_pred)
 
-    # Metryki dla SDNN
-    mae_sdnn = mean_absolute_error(sdnn_test, sdnn_pred)
-    mse_sdnn = mean_squared_error(sdnn_test, sdnn_pred)
-    rmse_sdnn = np.sqrt(mse_sdnn)
-    r2_sdnn = r2_score(sdnn_test, sdnn_pred)
+        # Predykcja na zbiorze walidacyjnym
+        pred_result = model.predict(X_val)
 
-    # Metryki dla RMSSD
-    mae_rmssd = mean_absolute_error(rmssd_test, rmssd_pred)
-    mse_rmssd = mean_squared_error(rmssd_test, rmssd_pred)
-    rmse_rmssd = np.sqrt(mse_rmssd)
-    r2_rmssd = r2_score(rmssd_test, rmssd_pred)
+        # Zakładamy, że model zwraca jeden output w postaci dwóch wartości [sdnn, rmssd]
+        sdnn_pred = pred_result[:, 0]
+        rmssd_pred = pred_result[:, 1]
 
-    # Wyświetlanie wyników
+        # Prawdziwe wartości
+        sdnn_test = y_val[:, 0]
+        rmssd_test = y_val[:, 1]
+
+        create_scatter_line_plotsc("SDNN", sdnn_test, sdnn_pred)
+        create_scatter_line_plotsc("RMSSD", rmssd_test, rmssd_pred)
+
+        # Obliczanie metryk dla SDNN
+        mae_sdnn = mean_absolute_error(sdnn_test, sdnn_pred)
+        mse_sdnn = mean_squared_error(sdnn_test, sdnn_pred)
+        rmse_sdnn = np.sqrt(mse_sdnn)
+        r2_sdnn = r2_score(sdnn_test, sdnn_pred)
+
+        # Obliczanie metryk dla RMSSD
+        mae_rmssd = mean_absolute_error(rmssd_test, rmssd_pred)
+        mse_rmssd = mean_squared_error(rmssd_test, rmssd_pred)
+        rmse_rmssd = np.sqrt(mse_rmssd)
+        r2_rmssd = r2_score(rmssd_test, rmssd_pred)
+
+        # Dodawanie wyników do list
+        mae_sdnn_list.append(mae_sdnn)
+        mse_sdnn_list.append(mse_sdnn)
+        rmse_sdnn_list.append(rmse_sdnn)
+        r2_sdnn_list.append(r2_sdnn)
+
+        mae_rmssd_list.append(mae_rmssd)
+        mse_rmssd_list.append(mse_rmssd)
+        rmse_rmssd_list.append(rmse_rmssd)
+        r2_rmssd_list.append(r2_rmssd)
+
+    # Wyświetlanie średnich wyników
+    plt.show()
+    print("\n### Wyniki Średnie ###")
     print("### SDNN ###")
-    print(f"MAE: {mae_sdnn:.2f}")
-    print(f"MSE: {mse_sdnn:.2f}")
-    print(f"RMSE: {rmse_sdnn:.2f}")
-    print(f"R^2: {r2_sdnn:.2f}")
+    print(f"Średnie MAE: {np.mean(mae_sdnn_list):.2f} ± {np.std(mae_sdnn_list):.2f}")
+    print(f"Średnie MSE: {np.mean(mse_sdnn_list):.2f} ± {np.std(mse_sdnn_list):.2f}")
+    print(f"Średnie RMSE: {np.mean(rmse_sdnn_list):.2f} ± {np.std(rmse_sdnn_list):.2f}")
+    print(f"Średnie R^2: {np.mean(r2_sdnn_list):.2f} ± {np.std(r2_sdnn_list):.2f}")
 
     print("### RMSSD ###")
-    print(f"MAE: {mae_rmssd:.2f}")
-    print(f"MSE: {mse_rmssd:.2f}")
-    print(f"RMSE: {rmse_rmssd:.2f}")
-    print(f"R^2: {r2_rmssd:.2f}")
-
-    # # Wizualizacja wyników
-    # create_scatter_line_plotsc("SDNN", sdnn_test, sdnn_pred)
-    # create_scatter_line_plotsc("RMSSD", rmssd_test, rmssd_pred)
-
-    # Wywołanie funkcji dla SDNN i RMSSD
-    Gcreate_plots("SDNN", sdnn_test, sdnn_pred)
-    Gcreate_plots("RMSSD", rmssd_test, rmssd_pred)
+    print(f"Średnie MAE: {np.mean(mae_rmssd_list):.2f} ± {np.std(mae_rmssd_list):.2f}")
+    print(f"Średnie MSE: {np.mean(mse_rmssd_list):.2f} ± {np.std(mse_rmssd_list):.2f}")
+    print(f"Średnie RMSE: {np.mean(rmse_rmssd_list):.2f} ± {np.std(rmse_rmssd_list):.2f}")
+    print(f"Średnie R^2: {np.mean(r2_rmssd_list):.2f} ± {np.std(r2_rmssd_list):.2f}")
 
     pass
 
